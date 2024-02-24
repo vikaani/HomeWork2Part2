74d3bc86955d801c80706b9fea0cd0e8ad0270a3
Viktoria <viktoriaani22@gmail.com>
2024-02-24 19:27:10 +0200

    Complete task 2.2 (Screen1)

--- HomeWork2.playground/Contents.swift
+++ HomeWork2.playground/Contents.swift
@@ -142,25 +142,41 @@ let cityIndex = Int(arc4random() % UInt32(weatherInCities.count - 1))
 
 
 // виводимо ПОВНУ інформацію для кожного міста, що є у масиві weatherInCities
+
+// Вирішення завдання 2.2
 print("---------- ЕКРАН 1 ----------")
-for index in 2 ... 8 {
-    let _ = index
-    let weatherInfo = weatherInCities[0]
+for index in 0 ..< cityNames.count {
+    let cityName = cityNames[index]
+    let weatherInfo = Weather(
+        city: cityName,
+        temp: Double(arc4random() % 30) + kelvinZero,
+        tempMin: Double(arc4random() % 30) + kelvinZero,
+        tempMax: Double(arc4random() % 30) + kelvinZero
+    )
+    weatherInCities.append(weatherInfo)
+}
+
+for city in weatherInCities {
+    let weatherInfo = city
     let city = weatherInfo.0
     let kelvin = weatherInfo.1
     let kelvinMin = weatherInfo.2
     let kelvinMax = weatherInfo.3
-
+    
+    let fahrenheit = kelvin * (9.0/5.0) - 459.67
+    let celsius = kelvin - kelvinZero
+    let fahrenheitMin = kelvinMin * (9.0/5.0) - 459.67
+    let fahrenheitMax = kelvinMax * (9.0/5.0) - 459.67
+    
     print("\n\(city):")
-    print("t: \(kelvin) C")
-    print("t: \(kelvin) F")
-    print("min t: \(kelvinMin) C / \(kelvinMin) F")
-    print("max t: \(kelvinMax) C / \(kelvinMax) F")
+    print("t: \(celsius) C")
+    print("t: \(String(format: "%.1f", fahrenheit)) F")
+    print("min t: \(kelvinMin - kelvinZero) C / \(String(format: "%.1f", fahrenheitMin)) F")
+    print("max t: \(kelvinMax - kelvinZero) C / \(String(format: "%.1f", fahrenheitMax)) F")
 }
+
 print("\n-----------------------------")
 print("\n\n")
-
-
 
 // виводимо інформацію про температуру тільки у Цельсій
 // для кожного міста, що є у масиві weatherInCities
